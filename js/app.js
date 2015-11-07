7e8906d3859629ff4f772e0564d9ef51daf84da8
Andy Nguyen <nguyenandyh@gmail.com>
2015-11-07 15:52:31 -0800

    fix: Add boundary for player movement
    Player does not exit the play area and movement into river will reset to initial position.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 
 // Constant variables for movement control
-var XMOVE = 100;
+var XMOVE = 101;
 var YMOVE = 83;
 
 // Initial start position of player
-var PSTARTX = 202;
+var PSTARTX = 203;
 var PSTARTY = 380;
+
+// Boundary of player so it stays on grass or stone
+var MINPX = 1;
+var MINPY = 48;
+var MAXPX = 405;
+var MAXPY = 380;
 
 // Offset pixels for player sprite
 var POFFSETX = 15;
@@ -60,12 +66,7 @@ var player = function(x,y) {
     this.sprite = 'images/char-boy.png';
 };
 
-// Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
 player.prototype.update = function() {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     return true;
 };
 
@@ -77,21 +78,27 @@ player.prototype.render = function() {
 // Input handler for the player
 // Parameter: keyDirection, one of four directions for character movement
 player.prototype.handleInput = function(keyDirection) {
+    // Coordinates
+
     switch (keyDirection) {
         case 'left':
-            this.x = this.x - XMOVE;
+            this.x = (this.x != MINPX) ? this.x - XMOVE : this.x;
             break;
         case 'up':
-            this.y = this.y - YMOVE;
+            if (this.y == MINPY) {
+                this.y = PSTARTY;
+                this.x = PSTARTX;
+            }
+            else this.y = this.y - YMOVE;
             break;
         case 'right':
-            this.x = this.x + XMOVE;
+            this.x = (this.x != MAXPX) ? this.x + XMOVE : this.x;
             break;
         case 'down':
-            this.y = this.y + YMOVE;
+            this.y = (this.y != MAXPY) ? this.y + YMOVE : this.y;
             break;
-
     }
+
 };
 
 // Now instantiate your objects.
