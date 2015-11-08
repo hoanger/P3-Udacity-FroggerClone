366e497ea7b8eebf380f930ce47c467e1cabd5e0
Andy Nguyen <nguyenandyh@gmail.com>
2015-11-07 22:47:40 -0800

    feat: Add player skin selection and multiple enemies
    Option to change player skin added. Multiple enemies have random speed which updates on reset.

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var MAXPX = 404;
 var MAXPY = 375;
 
 // Offset pixels for player sprite
-var POFFSETX = 75;
+var POFFSETX = 50;
 var POFFSETY = 60;
 
 // Start and end for enemy
@@ -22,15 +22,18 @@ var ENEMYEND = 505;
 var ENEMYSTART = -100;
 
 // Offset pixels for enemy sprite
-var EOFFSETX = 80;
+var EOFFSETX = 70;
 var EOFFSETY = 2;
+
+// Maximum number of enemies in game
+var MAXENEMIES = 4;
 
 // Array of bug enemies
 var allEnemies = [];
 
 
 // Enemies our player must avoid
-var Enemy = function(speed) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -38,7 +41,6 @@ var Enemy = function(speed) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.reset();
-    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -51,14 +53,22 @@ Enemy.prototype.update = function(dt) {
     if (this.x >= ENEMYEND) {
         this.reset();
     }
+    // Collision detected if enemy is same row as player AND the left and right sides of enemy and player are within bounds
+    if (player.y == (this.y - EOFFSETY)) {
+        if (((player.x <= (this.x + EOFFSETX)) && (player.x >= this.x)) || (((player.x + POFFSETX) >= this.x) && (player.x<= this.x))) {
+            console.log("Game Over");
+            player.reset();
+        }
+    }
 };
 
 // Initiate or reset enemy position
 Enemy.prototype.reset = function() {
     this.x = ENEMYSTART; // return enemy to the left side
     //put enemy in random stone row
-    var randomrow = generateRandom(3,0);
+    var randomrow = generateRandom(0,3);
     this.y = 45 + randomrow * YMOVE;
+    this.speed = generateRandom(2,9) * 50;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -72,23 +82,18 @@ Enemy.prototype.render = function() {
 var Player = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
+    this.reset();
     // The image/sprite for our player, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/char-cat-girl.png';
+    this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function() {
-    // TODO: change action to handle dead player
-    var badguy = allEnemies[0];
-    if (this.y == (badguy.y - EOFFSETY)) {
-        //console.log("! collision course !");
-        if (((this.x <= (badguy.x + EOFFSETX)) && (this.x >= badguy.x)) || (((this.x + POFFSETX) >= badguy.x) && (this.x<= badguy.x))) {
-            console.log("Die jor!");
-        };
-    };
-    console.log("No more die-----------")
+Player.prototype.update = function() {};
+
+Player.prototype.reset = function() {
+    this.x = PSTARTX;
+    this.y = PSTARTY;
+    console.log('Go!');
 };
 
 // Draw the player on the screen, required method for game
@@ -98,19 +103,20 @@ Player.prototype.render = function() {
 
 // Input handler for the player
 // Parameter: keyDirection, one of four directions for character movement
-Player.prototype.handleInput = function(keyDirection) {
-    switch (keyDirection) {
+Player.prototype.handleInput = function(keyInput) {
+    switch (keyInput) {
         case 'left':
             this.x = (this.x != MINPX) ? this.x - XMOVE : this.x;
             break;
         case 'up':
             if (this.y == MINPY) {
+                console.log("Winner winner chicken dinner");
                 this.y = PSTARTY;
                 this.x = PSTARTX;
             }
             else {
                 this.y = this.y - YMOVE;
-            };
+            }
             break;
         case 'right':
             this.x = (this.x != MAXPX) ? this.x + XMOVE : this.x;
@@ -118,24 +124,38 @@ Player.prototype.handleInput = function(keyDirection) {
         case 'down':
             this.y = (this.y != MAXPY) ? this.y + YMOVE : this.y;
             break;
+        case 'boy':
+            this.sprite = 'images/char-boy.png';
+            break;
+        case 'cat-girl':
+            this.sprite = 'images/char-cat-girl.png';
+            break;
+        case 'horn-girl':
+            this.sprite = 'images/char-horn-girl.png';
+            break;
+        case 'pink-girl':
+            this.sprite = 'images/char-pink-girl.png';
+            break;
+        case 'princess-girl':
+            this.sprite = 'images/char-princess-girl.png';
+            break;
     }
 
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var enemy = new Enemy(50)
-allEnemies.push(enemy);
-
-// Place player
-var player = new Player(PSTARTX, PSTARTY);
-
-function generateRandom(x,y) {
-    return Math.floor(Math.random() * x + y);
+for (i = 0; i < MAXENEMIES; i++) {
+    allEnemies.push(new Enemy());
 }
 
 
+// Place the player object in a variable called player
+var player = new Player();
+
+function generateRandom(x,y) {
+    return Math.floor(Math.random() * y + x);
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -144,7 +164,12 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        49: 'boy',
+        50: 'cat-girl',
+        51: 'horn-girl',
+        52: 'pink-girl',
+        53: 'princess-girl'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
